94fdb18941d10b39b6bb3934c6fc00940a2489a4
XuJun <nankaimathxujun@gmail.com>
2017-02-13 19:18:23 +0800

    add

--- FNNM.m
+++ FNNM.m
@@ -16,8 +16,8 @@ epsl = 1e-5;
 
 seta = mean(mean(Y.^2, 1));
 lambda = Par.lambdac * Par.c * NSig^2 / sqrt(seta) + epsl;
-lambda1 = lambda * col / Par.rank;
-lambda2 = lambda * row / Par.rank;
+% lambda1 = lambda * col / Par.rank;
+% lambda2 = lambda * row / Par.rank;
 
 %% Alternate Direction Optimization
 f_curr = 0;
@@ -25,18 +25,18 @@ for i = 1:maxiter
     f_prev = f_curr;
     
     % Fix U and update V
-    VT = (U' * U + lambda2 * eye(Par.rank)) \ (U' * Y);
+    VT = (U' * U + lambda * eye(Par.rank)) \ (U' * Y);
     tempVT = OMG * VT + (1 - OMG) * tempVT;
     VT = tempVT;
     % Fix V and update U
-    U = (Y * VT') / (VT * VT' + lambda1 * eye(Par.rank));
+    U = (Y * VT') / (VT * VT' + lambda * eye(Par.rank));
     tempU = OMG * U + (1 - OMG) * tempU;
     U = tempU;
     % energy function
-    DT = 0.5 * norm(Y - U * VT, 'fro') ^ 2;
+    DT = norm(Y - U * VT, 'fro') ^ 2;
     %     DT = DT(:)'*DT(:);
-    RT = lambda1 * norm(U, 'fro') ^ 2 + lambda2 * norm(VT, 'fro') ^ 2;
-    f_curr = DT + RT;
+    RT = lambda * norm(U, 'fro') ^ 2 + lambda * norm(VT, 'fro') ^ 2;
+    f_curr = 0.5 * (DT + RT);
 %     fprintf('FNNM Energy, %d th: %2.8f\n', i, f_curr);
     if (abs(f_prev - f_curr) / f_curr < 0.001)
         break;

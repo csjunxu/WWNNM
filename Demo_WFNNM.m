3e08d0b773d11ba5ef0a2c4772288581c1e43a42
XuJun <nankaimathxujun@gmail.com>
2017-02-14 09:58:32 +0800

    add

--- Demo_WFNNM.m
+++ Demo_WFNNM.m
@@ -21,7 +21,7 @@ for delta = [0.1 0.08 0.06 0]
     Par.delta = delta;
     for rank = [10]
         Par.rank = rank;
-        for lamada = 0.55:0.05:0.8
+        for lamada = 0.56:0.05:0.8
             Par.lamada = lamada;
             for lambdac = 0.8
                 Par.lambdac = lambdac;
@@ -44,7 +44,7 @@ for delta = [0.1 0.08 0.06 0]
                     fprintf('The initial value of PSNR = %2.4f, SSIM = %2.4f \n', PSNR,SSIM);
                     %
                     time0 = clock;
-                    im_out = FNNM_DeNoising( Par.nim, Par.I, Par );                                %WNNM denoisng function
+                    im_out = WFNNM_DeNoising( Par.nim, Par.I, Par );                                %WNNM denoisng function
                     if size(Par.I,1) == 512
                         T512 = [T512 etime(clock,time0)];
                         fprintf('Total elapsed time = %f s\n', (etime(clock,time0)) );
@@ -72,7 +72,7 @@ for delta = [0.1 0.08 0.06 0]
                 sT256 = std(T256);
                 fprintf('The best PSNR result is at %d iteration. \n',idx);
                 fprintf('The average PSNR = %2.4f, SSIM = %2.4f. \n', mPSNR(idx),mSSIM);
-                name = sprintf(['FNNM_Sigma_1AG_nSig' num2str(nSig) '_delta' num2str(delta) '_rank' num2str(rank) '_lc' num2str(lambdac) '_ls' num2str(lamada) '.mat']);
+                name = sprintf(['WFNNM_Sigma_1AG_nSig' num2str(nSig) '_delta' num2str(delta) '_rank' num2str(rank) '_lc' num2str(lambdac) '_ls' num2str(lamada) '.mat']);
                 save(name,'nSig','PSNR','SSIM','mPSNR','mSSIM','mT512','sT512','mT256','sT256');
             end
         end

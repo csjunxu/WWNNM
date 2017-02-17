d6eb546a8b841771ba2073b8bb09efeb901707f5
XuJun <nankaimathxujun@gmail.com>
2017-02-17 17:12:38 +0800

    add

--- WNNM_code/Demo_RID.m
+++ WNNM_code/Demo_RID.m
@@ -11,23 +11,23 @@ GT_im_dir  = dir(GT_fpath);
 TT_im_dir  = dir(TT_fpath);
 im_num = length(TT_im_dir);
 
-for nSig  = [16 17 18 19 21 22 23]
+for nSig  = 0.1:0.1:0.3
     
     PSNR = [];
     SSIM = [];
     for i = 1:im_num
-        O_Img = double( imread(fullfile(GT_Original_image_dir, GT_im_dir(i).name)) );
-        N_Img = double(imread(fullfile(TT_Original_image_dir, TT_im_dir(i).name) ));
-        fprintf( 'Noisy Image: nSig = %2.3f, PSNR = %2.2f \n\n\n', nSig, csnr( N_Img, O_Img, 0, 0 ) );
+        O_Img = im2double( imread(fullfile(GT_Original_image_dir, GT_im_dir(i).name)) );
+        N_Img = im2double(imread(fullfile(TT_Original_image_dir, TT_im_dir(i).name) ));
+        fprintf( 'Noisy Image: nSig = %2.3f, PSNR = %2.2f \n\n\n', nSig, csnr( N_Img * 255, O_Img * 255, 0, 0 ) );
         
         Par   = ParSet(nSig);
         Par.patsize       =   6;                            % Patch size
         Par.SearchWin =   20;
         Par.Iter          =   4;                            % total iter numbers
         Par.step = floor((Par.patsize) - 1);
-        E_Img = WNNM_DeNoising( N_Img, O_Img, Par );                                %WNNM denoisng function
-        PSNR = [PSNR csnr( O_Img, E_Img, 0, 0 )];
-        SSIM  = [SSIM cal_ssim( O_Img, E_Img, 0, 0 )];
+        E_Img = WNNM_DeNoising( N_Img * 255, O_Img * 255, Par );                                %WNNM denoisng function
+        PSNR = [PSNR csnr( O_Img * 255, E_Img * 255, 0, 0 )];
+        SSIM  = [SSIM cal_ssim( O_Img * 255, E_Img * 255, 0, 0 )];
         fprintf( 'Estimated Image: nSig = %2.3f, PSNR = %2.2f \n\n\n', nSig, PSNR(end) );
     end
     mPSNR=mean(PSNR);

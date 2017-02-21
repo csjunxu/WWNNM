ffe485f14072da0642df5bcf5ff8b2bd72cb244c
XuJun <nankaimathxujun@gmail.com>
2017-02-21 21:00:10 +0800

    add

--- WNNM_code/Demo_RID.m
+++ WNNM_code/Demo_RID.m
@@ -11,29 +11,26 @@ GT_im_dir  = dir(GT_fpath);
 TT_im_dir  = dir(TT_fpath);
 im_num = length(TT_im_dir);
 
-for nSig  = 0.02:0.02:0.1
-    
-    PSNR = [];
-    SSIM = [];
-    for i = 1:im_num
-        O_Img = im2double( imread(fullfile(GT_Original_image_dir, GT_im_dir(i).name)) );
-        N_Img = im2double(imread(fullfile(TT_Original_image_dir, TT_im_dir(i).name) ));
-        fprintf( 'Noisy Image: nSig = %2.3f, PSNR = %2.2f \n\n\n', nSig, csnr( N_Img * 255, O_Img * 255, 0, 0 ) );
-        
-        Par   = ParSet(nSig);
-        Par.patsize       =   6;                            % Patch size
-        Par.SearchWin =   20;
-        Par.Iter          =   4;                            % total iter numbers
-        Par.step = floor((Par.patsize) - 1);
-        E_Img = WNNM_DeNoising( N_Img, O_Img, Par );                                %WNNM denoisng function
-        PSNR = [PSNR csnr( O_Img * 255, E_Img * 255, 0, 0 )];
-        SSIM  = [SSIM cal_ssim( O_Img * 255, E_Img * 255, 0, 0 )];
-        fprintf( 'Estimated Image: nSig = %2.3f, PSNR = %2.2f \n\n\n', nSig, PSNR(end) );
-    end
-    mPSNR=mean(PSNR);
-    mSSIM=mean(SSIM);
-    fprintf('The average PSNR = %2.4f, SSIM = %2.4f. \n', mPSNR,mSSIM);
-    name = sprintf(['WNNM_RID_ImNum' num2str(im_num) '_nSig' num2str(nSig) '.mat']);
-    save(name,'nSig','PSNR','SSIM','mPSNR','mSSIM');
+PSNR = [];
+SSIM = [];
+for i = 1:im_num
+    O_Img = im2double( imread(fullfile(GT_Original_image_dir, GT_im_dir(i).name)) );
+    N_Img = im2double(imread(fullfile(TT_Original_image_dir, TT_im_dir(i).name) ));
+    nSig = NoiseEstimation(N_Img * 255, 6);
+    fprintf( 'Noisy Image: nSig = %2.3f, PSNR = %2.2f \n\n\n', nSig, csnr( N_Img * 255, O_Img * 255, 0, 0 ) );
+    Par   = ParSet(nSig);
+    Par.patsize       =   6;                            % Patch size
+    Par.SearchWin =   20;
+    Par.Iter          =   4;                            % total iter numbers
+    Par.step = floor((Par.patsize) - 1);
+    E_Img = WNNM_DeNoising( N_Img, O_Img, Par );                                %WNNM denoisng function
+    PSNR = [PSNR csnr( O_Img * 255, E_Img * 255, 0, 0 )];
+    SSIM  = [SSIM cal_ssim( O_Img * 255, E_Img * 255, 0, 0 )];
+    fprintf( 'Estimated Image: nSig = %2.3f, PSNR = %2.2f \n\n\n', nSig, PSNR(end) );
 end
+mPSNR=mean(PSNR);
+mSSIM=mean(SSIM);
+fprintf('The average PSNR = %2.4f, SSIM = %2.4f. \n', mPSNR,mSSIM);
+name = sprintf(['../WNNM_NL_RID_CC_' num2str(im_num) '_nSig' num2str(nSig) '.mat']);
+save(name,'nSig','PSNR','SSIM','mPSNR','mSSIM');
 
